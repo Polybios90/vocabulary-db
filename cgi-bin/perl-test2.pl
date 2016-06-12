3d36813d06fbe79a9101d65abe32ef432c7eb00a
v.reuter <v.reuter@dkfz-heidelberg.de>
2016-06-12 20:44:57 +0200

    perl script modified

--- cgi-bin/perl-test2.pl
+++ cgi-bin/perl-test2.pl
@@ -10,7 +10,6 @@ use DBI;
 use CGI;
 use CGI::Carp 'fatalsToBrowser';
 use base 'HTTP::Response';
-use Data::Dumper;
 
 use XML::Writer;
 use XML::Simple;
@@ -28,36 +27,52 @@ my $xs = XML::Simple->new;
 ### receive xml package
 
 
-my @params = $q->param();
+my @params = $q->param(); #only works for http post? Because param contains HTTP body
+
+my $table = $q->param($params[0]);
 
 
 
-### send response
+# query request
 
-# 
+my %headers = map { $_ => $q->http($_) } $q->http();
 
 
+
+# query HTTP request
 if(@params){
-     print $q->header;
-	 print $params[0];
-	 # say for @params;
+	 if($params[0] eq '')
+	 {
+		print $q->header('text/plain');
+		print "Got empty HTTP request:\n";
+	 }
+	 else
+	 {
+	    print $q->header;
+	 	print "<br>$params[0]  =  $table<br>";
+		# say for @params;
+	 }
 }
 else{
 	my $xml = '<?xml version="1.0" encoding="UTF-8"?> <data_body> <table>business_english</table><language>english</language><word1>bias</word1><translation>deutsch</translation><word2>Tendenz</word2></data_body>';
 	print $q->header('text/xml');
+	print "server: no parameters received..."
 }
 
 
 my $outputHash = {
 	string => 'Hello from perl',
+	remark => "extra info",
 };
+
+### send xml response response
 
 $hr->message($xs->XMLout($outputHash, KeyAttr => 'opt'));
 print $hr->as_string(); 
 
 ### db access & sql calls
 
-my $dsn = "dbi:mysql:vokabeln\@localhost:3306";
+my $dsn = "dbi:SQLite:vokabeln.sq3";
 my $user     = "root";
 my $password = "mn9vt3an10";
 my $dbh = DBI->connect($dsn, $user, $password, {
@@ -66,8 +81,26 @@ RaiseError       => 1,
 AutoCommit       => 1,
 FetchHashKeyName => 'NAME_lc',
 });
+if ($DBI::errstr)
+{
+	print $DBI::errstr;
+}
+
+# database queries and responses
+
+my $sql = 'SELECT english, deutsch FROM business_english WHERE id >= ? AND id < ?';
+my $sth = $dbh->prepare($sql);
+$sth->execute(1, 10);
+while (my @row = $sth->fetchrow_array) {
+   # print "english: $row[0]  deutsch: $row[1]\n";
+}
  
-# ...
+$sth->execute(12, 17);
+while (my $row = $sth->fetchrow_hashref) {
+   print "english: $row->{english}  deutsch: $row->{deutsch}\n";
+}
+
+
  
 $dbh->disconnect;
 
